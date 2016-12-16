413e21165b053f0a88ef2321f2778c6239794aff
mcardenas <marlonca@ucm.es>
2016-12-16 17:13:33 +0100

    add language, english and spanish

--- js/utils.js
+++ js/utils.js
@@ -1,68 +1,28 @@
-/**
- * Created by mcardenas on 24/10/16.
- */
-/* set active class for page
- =============================================*/
-function setClassMenu() {
-    var class_ = $("#header-include").attr('class');
-    if (class_ == 'index'){
-        $("#index").addClass("active");
-    }
-    if (class_ == 'about'){
-        $("#about").addClass("active");
-    }
-    if (class_ == 'proyectos'){
-        $("#proyectos").addClass("active");
-    }
-    if (class_ == 'simulaciones'){
-        $("#simulaciones").addClass("active");
-    }
-    if (class_ == 'tecnologia'){
-        $("#tecnologia").addClass("active");
-    }
-    if (class_ == 'licencia'){
-        $("#licencia").addClass("active");
-    }
-    if (class_ == 'contactanos'){
-        $("#contactanos").addClass("active");
-    }
-}
-
+/*
+*
+*/
 $(function () {
     w3IncludeHTML();
-    setClassMenu();
-
-    /*$('.micarrousel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay:true,
-        arrows: true,
-        centerMode:true,
-        dots: true,
-        infinite: true,
-        centerPadding:'40%',
-        adaptiveHeight: true,
-        variableWidth:true,
-        accesibility:true
-    });*/
 });
 
 $(function() {
     var showcase = $("#showcase");
 
-    showcase.Cloud9Carousel( {
-        yOrigin: 42,
-        yRadius: 30,
-        itemClass: "card",
-        buttonLeft: $(".nav_card.left_card"),
-        buttonRight: $(".nav_card.right_card"),
-        bringToFront: true,
-        onLoaded: function() {
-            showcase.css( 'visibility', 'visible' );
-            showcase.css( 'display', 'none' );
-            showcase.fadeIn( 1500 )
-        }
-    });
+    if (showcase != undefined) {
+        showcase.Cloud9Carousel( {
+            yOrigin: 42,
+            yRadius: 30,
+            itemClass: "card",
+            buttonLeft: $(".nav_card.left_card"),
+            buttonRight: $(".nav_card.right_card"),
+            bringToFront: true,
+            onLoaded: function() {
+                showcase.css( 'visibility', 'visible' );
+                showcase.css( 'display', 'none' );
+                showcase.fadeIn( 1500 )
+            }
+        });
+    }
 
     // Simulate physical button click effect
     $('.nav_card').click( function( e ) {
@@ -83,3 +43,39 @@ $(function() {
         }
     } )
 });
+
+$(document).ready(function() {
+    i18n.init(function(t) {
+        traslateLang('es');
+    });
+});
+
+function traslateLang(lang){
+    i18n.init(function(t) {
+        (lang == 'es') ? idioma = 'es': idioma = 'en';
+        var brand = $("#brand");
+        if (brand != undefined){
+            brand.i18n({lng: idioma});
+        }
+        var menu = $("#menu");
+        if (menu != undefined) {
+            menu.i18n({lng: idioma});
+        }
+        var home = $("#home");
+        if (home != undefined) {
+            home.i18n({lng: idioma});
+        }
+        var footer = $("#footer");
+        if (footer != undefined){
+            footer.i18n({lng: idioma});
+        }
+        var about = $("#about");
+        if (about != undefined) {
+            about.i18n({lng: idioma});
+        }
+        var casep = $("#casep");
+        if (casep != undefined){
+            casep.i18n({lng: idioma});
+        }
+    });
+}
